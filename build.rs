5a4856cd41ab2bb3bb5084e2fe5a9a6e61469d65
Andrew <kezin636@gmail.com>
2026-06-29 14:15:21 +0300

    I could try that with Mac

--- build.rs
+++ build.rs
@@ -11,14 +11,19 @@ fn main() {
         res.compile().expect("failed to compile Windows resources");
     }
 
-    #[cfg(unix)]
+    #[cfg(target_os = "linux")]
     if std::env::var("PROFILE").unwrap() == "release" {
-        copy_release_assets();
+        copy_linux_assets();
+    }
+
+    #[cfg(target_os = "macos")]
+    if std::env::var("PROFILE").unwrap() == "release" {
+        create_macos_app_bundle();
     }
 }
 
-#[cfg(unix)]
-fn copy_release_assets() {
+#[cfg(target_os = "linux")]
+fn copy_linux_assets() {
     use std::os::unix::fs::PermissionsExt;
     use std::path::PathBuf;
     use std::fs;
@@ -44,5 +49,68 @@ fn copy_release_assets() {
 
     println!("cargo:rerun-if-changed=scripts/fivetris.desktop");
     println!("cargo:rerun-if-changed=scripts/install.sh");
+    println!("cargo:rerun-if-changed=assets/icon.png");
+}
+
+#[cfg(target_os = "macos")]
+fn create_macos_app_bundle() {
+    use std::path::PathBuf;
+    use std::fs;
+
+    let manifest_dir = PathBuf::from(std::env::var("CARGO_MANIFEST_DIR").unwrap());
+    let out_dir = PathBuf::from(std::env::var("OUT_DIR").unwrap());
+    let release_dir = out_dir.ancestors().nth(3).unwrap().to_path_buf();
+
+    let app_dir = release_dir.join("fivetris.app");
+    let contents_dir = app_dir.join("Contents");
+    let macos_dir = contents_dir.join("MacOS");
+    let resources_dir = contents_dir.join("Resources");
+
+    fs::create_dir_all(&macos_dir).expect("failed to create MacOS directory");
+    fs::create_dir_all(&resources_dir).expect("failed to create Resources directory");
+
+    let binary_src = release_dir.join("fivetris");
+    let binary_dst = macos_dir.join("fivetris");
+    if binary_src.exists() {
+        fs::copy(&binary_src, &binary_dst)
+            .expect("failed to copy binary to .app bundle");
+    }
+
+    let icon_src = manifest_dir.join("assets/icon.png");
+    let icon_dst = resources_dir.join("icon.png");
+    if icon_src.exists() {
+        fs::copy(&icon_src, &icon_dst)
+            .expect("failed to copy icon to .app bundle");
+    }
+
+    let info_plist = contents_dir.join("Info.plist");
+    fs::write(&info_plist, r#"<?xml version="1.0" encoding="UTF-8"?>
+<!DOCTYPE plist PUBLIC "-//Apple//DTD PLIST 1.0//EN" "http://www.apple.com/DTDs/PropertyList-1.0.dtd">
+<plist version="1.0">
+<dict>
+    <key>CFBundleExecutable</key>
+    <string>fivetris</string>
+    <key>CFBundleIdentifier</key>
+    <string>io.github.relativemodder.fivetris</string>
+    <key>CFBundleName</key>
+    <string>Fivetris</string>
+    <key>CFBundleDisplayName</key>
+    <string>Fivetris</string>
+    <key>CFBundleVersion</key>
+    <string>0.3.0</string>
+    <key>CFBundleShortVersionString</key>
+    <string>0.3.0</string>
+    <key>CFBundlePackageType</key>
+    <string>APPL</string>
+    <key>CFBundleIconFile</key>
+    <string>icon.png</string>
+    <key>NSHighResolutionCapable</key>
+    <true/>
+    <key>LSMinimumSystemVersion</key>
+    <string>11.0</string>
+</dict>
+</plist>
+"#).expect("failed to write Info.plist");
+
     println!("cargo:rerun-if-changed=assets/icon.png");
 }
